30aaddbaad05c36105be4802ccc607344b8ada26
Vincent Esche <regexident@gmail.com>
2024-11-23 00:44:29 +0100

    Add `crate::prelude` module

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,11 @@ pub mod single_threaded;
 mod anchor;
 mod arena;
 
+pub mod prelude {
+    pub use crate::core::{
+        AnchorCore, AnchorHandle, DirtyHandle, Engine, OutputContext, UpdateContext,
+    };
+    pub use crate::{Anchor, MultiAnchor};
+}
+
 pub use self::anchor::*;
