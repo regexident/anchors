698134fc0d8ac3c7efa053947079f49ad21388c4
Vincent Esche <regexident@gmail.com>
2024-11-22 22:47:36 +0100

    Improve documentation formatting of `Node`

--- src/singlethread/node.rs
+++ src/singlethread/node.rs
@@ -7,22 +7,22 @@ use super::{generation::Generation, node_ptrs::NodePtrs, AnchorDebugInfo, Generi
 pub struct Node {
     pub observed: Cell<bool>,
 
-    /// bool used during height incrementing to check for loops
+    /// Bool used during height incrementing to check for loops
     pub visited: Cell<bool>,
 
-    /// number of nodes that list this node as a necessary child
+    /// Number of nodes that list `self` as a necessary child.
     pub necessary_count: Cell<usize>,
 
     pub token: u32,
 
     pub(super) debug_info: Cell<AnchorDebugInfo>,
 
-    /// tracks the generation when this Node last polled as Updated or Unchanged
+    /// Tracks when this `Node`` was last polled as `Updated` or `Unchanged`.
     pub(super) last_ready: Cell<Option<Generation>>,
-    /// tracks the generation when this Node last polled as Updated
+    /// Tracks when this `Node` was` last polled as `Updated`.
     pub(super) last_update: Cell<Option<Generation>>,
 
-    /// Some() if this node is still active, None otherwise
+    /// `Some(_)`` if this node is still active, `None`` otherwise
     pub(super) anchor: RefCell<Option<Box<dyn GenericAnchor>>>,
 
     pub ptrs: NodePtrs,
